4301d79012a38546724dbbee26fa47a789d085b6
AugustineEmejor <emejoraugustine@yahoo.com>
2022-12-08 18:59:23 -0800

    my hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #!/bin/bash
 #include <stdio.h>
+
 /**
- * main-Entry
- * Return:Always 0
- * /
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like a multi lingual puzzle");
-	return(0);
+	puts("/"Programming is like buliding a multi lingual puzzle");
+	return (0);
 }
